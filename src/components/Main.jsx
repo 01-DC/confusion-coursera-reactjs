f67d32f015451fa14c35fb473de162dbdf21a434
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-06-28 22:21:35 +0530

    add Contact component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,9 +4,9 @@ import { Route, Routes } from "react-router-dom"
 import { DISHES } from "../data/dishes"
 import Home from "./Home"
 import Menu from "./Menu"
-import DishDetail from "./DishDetail"
 import Header from "./Header"
 import Footer from "./Footer"
+import Contact from "./Contact"
 
 function Main() {
 	const [dishes, setDishes] = useState(DISHES)
@@ -17,6 +17,7 @@ function Main() {
 			<Routes>
 				<Route path="/home" element={<Home />} />
 				<Route exact path="/menu" element={<Menu dishes={dishes} />} />
+				<Route exact path="/contactus" element={<Contact />} />
 				<Route path="*" element={<Home />} />
 			</Routes>
 			<Footer />
